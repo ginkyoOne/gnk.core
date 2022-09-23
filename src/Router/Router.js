ac8c597ba9b061bbbac7fa5227c1968bcf8324f8
ginkyoOne <cadmartins@hotmail.com>
2022-09-23 16:35:02 +0200

    404 page not found

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,3 +1,50 @@
+/* import Vue from "vue";
+import VueRouter from "vue-router";
+
+import LandingPage from "./LandingPage.vue";
+import LoadingComponent from "./LoadingComponent.vue";
+import ErrorComponent from "./ErrorComponent.vue";
+
+const lazyLoadView = ({ component, loading, error }) => {
+  const AsyncHandler = () => ({
+    component,
+    loading,
+    error
+  });
+
+  return () =>
+    Promise.resolve({
+      functional: true,
+      render(h, { data, children }) {
+        return h(AsyncHandler, data, children);
+      }
+    });
+};
+
+const Profile = lazyLoadView({
+  component: import("./Profile.vue"),
+  loading: LoadingComponent,
+  error: ErrorComponent
+});
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: LandingPage },
+  { path: "/profile", component: Profile }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
+ */
+
+
+
+
+
 import { createWebHistory, createRouter } from "vue-router";
 
 let router;
